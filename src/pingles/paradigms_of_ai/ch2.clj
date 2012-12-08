e4981d4749cdb4aae5f62c3be62d59817f51008a
Paul Ingles <paul@oobaloo.co.uk>
2012-12-08 17:44:46 +0000

    add the larger grammar from ch2

--- src/pingles/paradigms_of_ai/ch2.clj
+++ src/pingles/paradigms_of_ai/ch2.clj
@@ -7,6 +7,25 @@
                            Noun [man ball woman table]
                            Verb [hit took saw liked]})
 
+(def bigger-grammar '{sentence [[noun-phrase verb-phrase]]
+                      noun-phrase [[Article Adj* Noun PP*] [Name] [Pronoun]]
+                      verb-phrase [[Verb noun-phrase PP*]]
+                      PP* [[] [PP PP*]]
+                      Adj* [[] [Adj Adj*]]
+                      PP [[Prep noun-phrase]]
+
+                      Prep [to in by with on]
+                      Adj [big little blue green adiabatic]
+                      Article [the a]
+                      Name [Pat Kim Lee Terry Robin]
+                      Noun [man ball woman table]
+                      Verb [hit took saw liked]
+                      Pronoun [he she it these those that]})
+
+(defmacro with-grammar [grammar & body]
+  `(binding [*grammar* ~grammar]
+     ~@body))
+
 (defn rule-lhs
   [rule]
   (first rule))
@@ -25,4 +44,8 @@
 (comment
   (generate 'sentence)
   ; a man took a ball
+
+  (with-grammar bigger-grammar
+    (generate 'sentence))
+  ;(a man liked those)
   )
